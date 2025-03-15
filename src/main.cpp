15aa070cf5d3bbe2091acba3596be8a37d7d6976
Hepodon <brandonlewis0120@gmail.com>
2025-03-15 14:53:13 -0500

    Added Arcs for Temp and RPM UNTESTED

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "pros/misc.h"
 
 using namespace pros;
+using namespace std;
 
 Controller conInput(E_CONTROLLER_MASTER);
 Motor motors[20] = {Motor(1),  Motor(2),  Motor(3),  Motor(4),  Motor(5),
@@ -24,25 +25,66 @@ static void MotorSpeedUp(lv_event_t *e) { speed++; }
 static void MotorSpeedDown(lv_event_t *e) { speed--; }
 static void MotorSpeedReset(lv_event_t *e) { speed = 0; }
 
+lv_obj_t *motorRPMArc, *motorTempArc;
+
+void update_arc_color(lv_obj_t *arc, int value, int maxValue) {
+  lv_color_t color =
+      lv_palette_main(value > maxValue * 0.75   ? LV_PALETTE_RED
+                      : value > maxValue * 0.5  ? LV_PALETTE_ORANGE
+                      : value > maxValue * 0.25 ? LV_PALETTE_YELLOW
+                                                : LV_PALETTE_GREEN);
+  lv_obj_set_style_arc_color(arc, color, LV_PART_INDICATOR);
+}
 // Function to create the UI for the selected motor
 void create_Motor_UI() {
+
   lv_obj_clean(lv_scr_act()); // Clear the screen before updating
+
+  // Check if motor is connected by verifying temperature reading
+  if (motors[selectedPort - 1].get_temperature() == PROS_ERR_F) {
+    lv_label_set_text(
+        motorLabel,
+        ("Port " + to_string(selectedPort) + ": No motor connected").c_str());
+    return;
+  }
+  motorRPMArc = lv_arc_create(lv_scr_act());
+  lv_arc_set_range(motorRPMArc, 0, 200);
+  lv_obj_set_size(motorRPMArc, 120, 120);
+  lv_obj_align(motorRPMArc, LV_ALIGN_LEFT_MID, 40, 0);
+  lv_arc_set_rotation(motorRPMArc, 270);
+  lv_arc_set_bg_angles(motorRPMArc, 180, 360);
+  lv_arc_set_mode(motorRPMArc, LV_ARC_MODE_NORMAL);
+  lv_obj_clear_flag(motorRPMArc, LV_OBJ_FLAG_CLICKABLE);
+
+  motorTempArc = lv_arc_create(lv_scr_act());
+  lv_arc_set_range(motorTempArc, 0, 100);
+  lv_obj_set_size(motorTempArc, 120, 120);
+  lv_obj_align(motorTempArc, LV_ALIGN_RIGHT_MID, -40, 0);
+  lv_arc_set_rotation(motorTempArc, 270);
+  lv_arc_set_bg_angles(motorTempArc, 180, 360);
+  lv_arc_set_mode(motorTempArc, LV_ARC_MODE_NORMAL);
+  lv_obj_clear_flag(motorTempArc, LV_OBJ_FLAG_CLICKABLE);
   lv_obj_t *label;
   lv_obj_t *SpdUPButton = lv_obj_create(lv_scr_act()),
            *SpdDOWNButton = lv_obj_create(lv_scr_act());
   lv_obj_t *SpdRESETButton = lv_obj_create(lv_scr_act());
+
   lv_obj_align(SpdUPButton, LV_ALIGN_TOP_LEFT, 320, 40);
   lv_obj_align(SpdRESETButton, LV_ALIGN_TOP_LEFT, 320, 120);
   lv_obj_align(SpdDOWNButton, LV_ALIGN_TOP_LEFT, 320, 160);
+
   lv_obj_set_size(SpdUPButton, 95, 75);
   lv_obj_set_size(SpdRESETButton, 95, 35);
   lv_obj_set_size(SpdDOWNButton, 95, 75);
+
   lv_obj_set_style_radius(SpdUPButton, 15, 0);
   lv_obj_set_style_radius(SpdRESETButton, 15, 0);
   lv_obj_set_style_radius(SpdDOWNButton, 15, 0);
+
   lv_obj_add_event_cb(SpdRESETButton, MotorSpeedReset, LV_EVENT_PRESSING, NULL);
   lv_obj_add_event_cb(SpdUPButton, MotorSpeedUp, LV_EVENT_PRESSING, NULL);
   lv_obj_add_event_cb(SpdDOWNButton, MotorSpeedDown, LV_EVENT_PRESSING, NULL);
+
   lv_obj_set_style_bg_color(SpdUPButton, lv_palette_main(LV_PALETTE_LIGHT_BLUE),
                             LV_STATE_DEFAULT);
   lv_obj_set_style_bg_color(SpdDOWNButton, lv_palette_main(LV_PALETTE_BLUE),
@@ -71,26 +113,18 @@ void create_Motor_UI() {
   lv_obj_align(title, LV_ALIGN_TOP_MID, 0, 10);
 
   motorLabel = lv_label_create(lv_scr_act());
-  std::string motorText = "Port " + std::to_string(selectedPort);
+  string motorText = "Port " + to_string(selectedPort);
   lv_label_set_text(motorLabel, motorText.c_str());
   lv_obj_align(motorLabel, LV_ALIGN_TOP_MID, 0, 40);
+  /*
+    motorRPM = lv_label_create(lv_scr_act());
+    lv_label_set_text(motorRPM, "RPM: ");
+    lv_obj_align(motorRPM, LV_ALIGN_TOP_LEFT, 10, 80);
 
-  // Check if motor is connected by verifying temperature reading
-  if (motors[selectedPort - 1].get_temperature() == PROS_ERR_F) {
-    lv_label_set_text(motorLabel, ("Port " + std::to_string(selectedPort) +
-                                   ": No motor connected")
-                                      .c_str());
-    return;
-  }
-
-  motorRPM = lv_label_create(lv_scr_act());
-  lv_label_set_text(motorRPM, "RPM: ");
-  lv_obj_align(motorRPM, LV_ALIGN_TOP_LEFT, 10, 80);
-
-  motorTemp = lv_label_create(lv_scr_act());
-  lv_label_set_text(motorTemp, "Temp: ");
-  lv_obj_align(motorTemp, LV_ALIGN_TOP_LEFT, 10, 120);
-
+    motorTemp = lv_label_create(lv_scr_act());
+    lv_label_set_text(motorTemp, "Temp: ");
+    lv_obj_align(motorTemp, LV_ALIGN_TOP_LEFT, 10, 120);
+  */
   motorPower = lv_label_create(lv_scr_act());
   lv_label_set_text(motorPower, "Power: ");
   lv_obj_align(motorPower, LV_ALIGN_TOP_LEFT, 10, 160);
@@ -106,7 +140,13 @@ void update_Motor_Data() {
     return;
 
   char buffer[50];
+  int rpm = motors[selectedPort - 1].get_actual_velocity();
+  int temp = motors[selectedPort - 1].get_temperature();
+  lv_arc_set_value(motorRPMArc, rpm);
+  lv_arc_set_value(motorTempArc, temp);
 
+  update_arc_color(motorRPMArc, rpm, 200);
+  update_arc_color(motorTempArc, temp, 100);
   sprintf(buffer, "RPM: %.1f", motors[selectedPort - 1].get_actual_velocity());
   lv_label_set_text(motorRPM, buffer);
 
